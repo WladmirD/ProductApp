b93af06c28b9654d19a645e00f3e3b0584f88692
WladmirD <wladmir.d@gmail.com>
2019-10-30 16:53:12 -0400

    Add entry to CRUD

--- models/product.model.js
+++ models/product.model.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ProductSchema = new Schema({
+    namelist: {type: String, required: true, max: 100},
+    creator:{type: int, required: true},
+    time:{type: int, required: true},
+    creatorOrder: {type: String, required: true, max: 100},
     name: {type: String, required: true, max: 100},
     size: {type: String, required: true, max: 100},
     flavour: {type: String, required: true, max: 100},
+    willbuy: {type: String, required: true, max: 100},
 
 });
 
