bbca80ca89d528e0f0d43bd67d8f64f39bd89090
WladmirD <wladmir.d@gmail.com>
2019-11-01 07:59:05 -0400

    Implementing orders creation

---
+++ node-rest-api-jwt/app/api/controllers/orders.js
@@ -0,0 +1,54 @@
+const orderModel = require('../models/orders');
+module.exports = {
+ getById: function(req, res, next) {
+  console.log(req.body);
+  orderModel.findById(req.params.orderId, function(err, orderInfo){
+   if (err) {
+    next(err);
+   } else {
+    res.json({status:"success", message: "order found!!!", data:{orders: orderInfo}});
+   }
+  });
+ },
+getAll: function(req, res, next) {
+  let ordersList = [];
+orderModel.find({}, function(err, orders){
+   if (err){
+    next(err);
+   } else{
+    for (let order of orders) {
+     ordersList.push({id: order._id, name: order.name, released_on: order.released_on});
+    }
+    res.json({status:"success", message: "Orders list found!!!", data:{orders: ordersList}});
+       
+   }
+});
+ },
+updateById: function(req, res, next) {
+  orderModel.findByIdAndUpdate(req.params.orderId,{name:req.body.name}, function(err, orderInfo){
+if(err)
+    next(err);
+   else {
+    res.json({status:"success", message: "Order updated successfully!!!", data:null});
+   }
+  });
+ },
+deleteById: function(req, res, next) {
+  orderModel.findByIdAndRemove(req.params.orderId, function(err, orderInfo){
+   if(err)
+    next(err);
+   else {
+    res.json({status:"success", message: "Order deleted successfully!!!", data:null});
+   }
+  });
+ },
+create: function(req, res, next) {
+  orderModel.create({ name: req.body.name, released_on: req.body.released_on }, function (err, result) {
+      if (err) 
+       next(err);
+      else
+       res.json({status: "success", message: "Order added successfully!!!", data: null});
+      
+    });
+ },
+}
---
+++ node-rest-api-jwt/routes/orders.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../app/api/controllers/orders');
+router.get('/', orderController.getAll);
+router.post('/', orderController.create);
+router.get('/:orderId', orderController.getById);
+router.put('/:orderId', orderController.updateById);
+router.delete('/:orderId', orderController.deleteById);
+module.exports = router;
